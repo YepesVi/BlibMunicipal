064cf15064314858c77f4913ce2fd59014e74a76
YepesVi <andresyepepesvi1@gmail.com>
2026-03-11 20:25:03 -0500

    initial structure backend

---
+++ MunicipalBack/src/test/java/com/Biblioteca/MunicipalBack/MunicipalBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Biblioteca.MunicipalBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MunicipalBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
